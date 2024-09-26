0df2e47d3c9acbe99adb6f851ab6af9c2f424fd3
Ryan Smith <ryan.d.smith.106@gmail.com>
2024-09-26 19:21:53 -0400

    CI: Prepare library for dynamic versioning

--- src/peculiar_audience/__init__.py
+++ src/peculiar_audience/__init__.py
@@ -1,3 +1,10 @@
 from ._iterable import *  # noqa: F401, F403
 from ._mapping import *  # noqa: F401, F403
 from ._sequence import *  # noqa: F401, F403
+
+try:
+    from .version import __version__
+except ImportError:
+    # version.py is generated at build time
+    # if for some reason it's missing use the same fallback PDM uses
+    __version__ = "0.0.0"
