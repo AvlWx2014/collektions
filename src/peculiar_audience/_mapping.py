5d940047ef64315fb8a312998d776bd62da7c774
Ryan Smith <ryan.d.smith.106@gmail.com>
2024-09-26 18:51:44 -0400

    Feature: Add map_keys and map_values families

--- src/peculiar_audience/_mapping.py
+++ src/peculiar_audience/_mapping.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 __all__ = ["filter_keys", "filter_values"]
 
+from collections.abc import MutableMapping
 from typing import Callable, Iterable, Mapping, MutableSequence
 
-from ._types import K, V
+from ._types import K, R, V
 
 
 def filter_keys(
@@ -41,3 +42,51 @@ def filter_values(
         if predicate(key, value):
             result.append(value)
     return result
+
+
+def map_keys(mapping: Mapping[K, V], transform: Callable[[K, V], R]) -> Mapping[R, V]:
+    """Return a new mapping with a key set formed by applying ``transform`` to the original key set.
+
+    The value set in the new mapping is the same as the value set in the original mapping.
+    """
+    return map_keys_to(mapping, {}, transform)
+
+
+def map_keys_to(
+    mapping: Mapping[K, V],
+    destination: MutableMapping[R, V],
+    transform: Callable[[K, V], R],
+) -> MutableMapping[R, V]:
+    """Update ``destination`` with ``mapping``.
+
+    Entries from the original ``mapping`` have their keys transformed by applying ``transform``,
+    and their values unchanged.
+    """
+    for key, value in mapping.items():
+        new_key = transform(key, value)
+        destination[new_key] = value
+    return destination
+
+
+def map_values(mapping: Mapping[K, V], transform: Callable[[K, V], R]) -> Mapping[K, R]:
+    """
+    Return a new mapping w/ a value set formed by applying ``transform`` to the original value set.
+
+    The key set in the new mapping is the same as the key set in the original mapping.
+    """
+    return map_values_to(mapping, {}, transform)
+
+
+def map_values_to(
+    mapping: Mapping[K, V],
+    destination: MutableMapping[K, R],
+    transform: Callable[[K, V], R],
+) -> MutableMapping[K, R]:
+    """Update ``destination`` with ``mapping``.
+
+    Entries from the original ``mapping`` have their keys unchanged, and their values transformed
+    by applying ``transform``.
+    """
+    for key, value in mapping.items():
+        destination[key] = transform(key, value)
+    return destination
--- tests/test__mapping.py
+++ tests/test__mapping.py
@@ -1,6 +1,9 @@
+import calendar
+
 from hamcrest import assert_that, equal_to
 
 from peculiar_audience import filter_keys, filter_values
+from peculiar_audience._mapping import map_keys, map_values
 
 
 def test_filter_keys():
@@ -14,4 +17,44 @@ def test_filter_values():
     mapping = dict((i, str(i)) for i in range(10))
     expected = ["0", "2", "4", "6", "8"]
     actual = filter_values(mapping, lambda _, v: int(v) % 2 == 0)
+    assert_that(actual, equal_to(expected))
+
+
+def test_map_keys_family():
+    input_ = {month: i for i, month in enumerate(calendar.month_name) if month}
+    expected = {
+        "JAN": 1,
+        "FEB": 2,
+        "MAR": 3,
+        "APR": 4,
+        "MAY": 5,
+        "JUN": 6,
+        "JUL": 7,
+        "AUG": 8,
+        "SEP": 9,
+        "OCT": 10,
+        "NOV": 11,
+        "DEC": 12,
+    }
+    actual = map_keys(input_, lambda k, _: k[:3].upper())
+    assert_that(actual, equal_to(expected))
+
+
+def test_map_values_family():
+    input_ = {i: month for i, month in enumerate(calendar.month_name) if month}
+    expected = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec",
+    }
+    actual = map_values(input_, lambda _, v: v[:3])
     assert_that(actual, equal_to(expected))
